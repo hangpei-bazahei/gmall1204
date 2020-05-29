6c1890badfe80857d877ceb145802ed1f76bf2c0
alanpei <allen.hangpei@gmail.com>
2020-05-28 23:35:27 -0400

    add modules

---
+++ gmall1204-user-manage/src/main/java/com/ph/gmall1204/gmall1204user/Gmall1204UserManageApplication.java
@@ -0,0 +1,13 @@
+package com.ph.gmall1204.gmall1204user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gmall1204UserManageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Gmall1204UserManageApplication.class, args);
+    }
+
+}
